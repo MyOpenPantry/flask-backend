7dc36bade93d3c908e8d87fc8eb5ff5717a70d39
Evan Campbell <evangarreth@gmail.com>
2021-03-30 17:39:01 -0500

    Make ingredient names unique

--- myopenpantry/models/ingredients.py
+++ myopenpantry/models/ingredients.py
@@ -8,7 +8,7 @@ class Ingredient(db.Model):
     __tablename__ = "ingredients"
 
     id = sa.Column(sa.Integer, primary_key=True)
-    name = sa.Column(sa.String(128), nullable=False)
+    name = sa.Column(sa.String(128), nullable=False, unique=True)
 
     # many to one, with Ingredient being the one 
     items = relationship('Item', back_populates="ingredient")
