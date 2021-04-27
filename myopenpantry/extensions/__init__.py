4f8bdff2edb31070d0b12e478b57e8dfc4bcd86f
Evan Campbell <evangarreth@gmail.com>
2021-04-27 14:56:13 -0500

    Add flask-cors dependency

--- myopenpantry/extensions/__init__.py
+++ myopenpantry/extensions/__init__.py
@@ -1,9 +1,12 @@
+from flask_cors import CORS
+
 from . import database
 from .api import Api
 
 def create_api(app):
 
     api = Api(app)
+    CORS(app)
 
     for extension in (database,):
         extension.init_app(app)
